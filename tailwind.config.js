993b9d39a133090582e7a6793b6101b24fda4f8a
ahmeteren.akkus <ahmeteren.akkus@std.antalya.edu.tr>
2023-10-15 14:36:00 +0300

    tailwind starter files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{html,js}"],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
